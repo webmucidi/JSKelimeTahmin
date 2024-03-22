0f48b1360a6b62da3e53c070fc9a9682e0d0457e
webmucidi <fatihcelik1907@gmail.com>
2024-03-22 14:58:09 +0300

    quiz22mart

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-//veri havuzu oluştur
 const ogrenciler = [
     { ad: "Enes", ipucu: "Hiperaktif" },
     { ad: "Bilal", ipucu: "Kumarbaz"  },
@@ -6,63 +5,93 @@ const ogrenciler = [
     { ad: "Tuğba", ipucu: "Sincap"  },
     { ad: "Erayinho", ipucu: "Okulu bıraktı"  }
 ];
-//nesneleri değişkene al
+
 const kapsayici = document.getElementById("container");
 const ipucuAlani = document.getElementById("hint");
-const tahminGirisi= document.getElementById("guessInput");
-const tahminButonu = document.getElementById("guessButton");
-const kalanHakAlani = document.getElementById("attempts");
 const sonucAlani = document.getElementById("result");
 
-//sayaçlar ve rastgele seçilen kelimeler için değiken oluştur
-let sayac=0;
-let rastgeleOgrenci={};
-let harfler=[];
+const guessArea = document.getElementById("guessArea");
+const aciklamalar = document.getElementById("attempts");
+const guessButton = document.getElementById("guessButton");
+const guessInput = document.getElementById("guessInput");
+console.log(aciklamalar,guessButton)
+let rastgeleOgrenci = {};
+let harfler = [];
 
-//Oyunu yükleme fonksiyonunu çağır
 oyunuBaslat();
 
-
-//Oyun yükleme fonksiyonu tanımla
-function oyunuBaslat(){
-    rastgeleOgrenci=ogrenciler[Math.floor(Math.random() * ogrenciler.length)];
+function oyunuBaslat() {
+    rastgeleOgrenci = ogrenciler[Math.floor(Math.random() * ogrenciler.length)];
     
-    ipucuAlani.textContent=`Acaba kim bu?: ${rastgeleOgrenci.ipucu}`;
+    ipucuAlani.innerHTML = "Acaba kim bu? " + rastgeleOgrenci.ipucu;
     kartlariOlustur();
-    sonucAlani.textContent="";
+    sonucAlani.textContent = "";
+    //tryAgainButton.style.display = "none";
+    guessArea.innerHTML = "";
+
+    aciklamalar.innerHTML="Doğru cevabı bulmak için harflere sırayla tıklayınız.";
+    guessButton.style.display="none";
+    guessInput.style.display="none";
+
+
 }
 
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
-//Kartları oluşturma fonksiyonunu tanımla
-function kartlariOlustur(){
-    sayac=0;
-    kapsayici.innerHTML="";
+function kartlariOlustur() {
+    kapsayici.innerHTML = "";
+    harfler = shuffle(rastgeleOgrenci.ad.toUpperCase().split(""));
     
-    //Önce kelimeyi parçala ve harfleri diziye at
-    harfler=rastgeleOgrenci.ad.toUpperCase().split("");
-    
-    harfler.forEach((harf,index)=>{
-        kart=document.createElement("div");
-        kart.innerHTML="?";
-        kart.className="card";
-        kart.dataset.value=harf;
-        kart.dataset.index=index;
+    harfler.forEach((harf, index) => {
+        let kart = document.createElement("div");
+        kart.innerHTML = harf;
+        kart.className = "card";
+        kart.dataset.value = harf;
+        kart.dataset.index = index;
         kapsayici.appendChild(kart);
-        kart.addEventListener("click",kartAc);
-    })
-
+        kart.addEventListener("click", kartAc);
+    });
 }
 
 
-//Kartları açmak için fonksiyonu tanımla
-function kartAc(){
+function kartAc() {
     
-    this.textContent=harfler[this.dataset.index];
-    this.classList.add("revealed");
+    guessArea.appendChild(this);
 
-}
+    let selectedLetters = [];
+    let guessAreaCards = guessArea.getElementsByClassName("card");
+    for (let i = 0; i < guessAreaCards.length; i++) {
+        selectedLetters.push(guessAreaCards[i].dataset.value);
+    }    
+    let guess = selectedLetters.join("");
 
-//Tahmin kontrol ve sonuç gösterme fonksiyonu tanımla
-function sonucKontrol(){
+    if (guess.length === harfler.length) {
+        if (guess === rastgeleOgrenci.ad.toUpperCase()) {
+            sonucAlani.textContent = "Tebrikler, doğru tahmin!";
+            for (let j = 0; j < guessAreaCards.length; j++) {
+                guessAreaCards[j].classList.add("revealed");
+                guessAreaCards[j].classList.remove("wrong");
+            }
 
+
+        } else {
+            sonucAlani.textContent = "Maalesef, yanlış tahmin. Tekrar deneyin!";
+            for (let j = 0; j < guessAreaCards.length; j++) {
+                guessAreaCards[j].classList.add("wrong");
+                guessAreaCards[j].classList.remove("revealed");
+            }
+
+        }
+        
+        guessButton.style.display = "block";
+        guessButton.disabled=false;
+        guessButton.innerHTML="Yeniden Başlat";
+        guessButton.addEventListener("click",oyunuBaslat);
+    }
 }
